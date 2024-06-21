550f25154cedee4d8a4de36edd7031cdb7f1d24b
winstonallo <arthurbiedchar@gmail.com>
2024-06-21 20:09:13 +0200

    add: basic build structure

---
+++ include/utils.h
@@ -0,0 +1,6 @@
+#ifndef UTILS_H
+#define UTILS_H
+
+void example();
+
+#endif  // EXAMPLE_H
---
+++ src/example.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+#include "utils.h"
+
+void example() {
+    std::cout << "Example" << std::endl;
+}
