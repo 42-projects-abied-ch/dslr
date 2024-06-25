2a9619b00ff8c9ddffcd757b62795a72bd62ab83
Arthur Bied-Charreton <arthurbiedchar@gmail.com>
2024-06-25 15:14:27 +0200

    add: dropping index in drop non numerical

--- src/scatter_plot.py
+++ src/scatter_plot.py
@@ -1,13 +1,29 @@
 from DataFrame import DataFrame
 import matplotlib.pyplot as plt
+from matplotlib import cm
+import numpy as np
 
-def scatter_plot(df: DataFrame) -> None:
-    df.drop_non_numerical()
-    df.print_describe()
-    for header in df._columns:
-        plt.scatter(df._columns[header].get_column_data(), df._columns[header].get_column_data())
-        plt.title(header)
-        plt.show
+def scatter_plot(df):
+    df.drop_non_numerical(index=False)  # Assuming this function properly cleans the DataFrame
+    headers = list(df._columns.keys())
+    
+    for i, x_header in enumerate(headers):
+        for y_header in headers[i+1:]:
+            x_data = df._columns[x_header].get_column_data()
+            y_data = df._columns[y_header].get_column_data()
+
+            # Plot x_data points in blue
+            plt.scatter(x_data, [0]*len(x_data), color='blue', label=x_header)  # Plotting at y=0 for visualization
+            
+            # Plot y_data points in red
+            plt.scatter(y_data, [1]*len(y_data), color='red', label=y_header)  # Plotting at y=1 for visualization
+            
+            # Adding labels and titles
+            plt.xlabel(x_header)
+            plt.ylabel(y_header)
+            plt.title(f'{x_header} vs {y_header}')
+            plt.legend()
+            plt.show()
 
 def main():
     df = DataFrame()
