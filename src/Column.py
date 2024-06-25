aa65d6196b9843259cf40160a9c78295124224fd
Артем Апенко <lbapart@gmail.com>
2024-06-25 10:15:32 +0200

    added describe part of project

--- src/Column.py
+++ src/Column.py
@@ -1,3 +1,6 @@
+import math
+from datetime import datetime
+
 INT = 'int'
 FLOAT = 'float'
 STRING = 'string'
@@ -11,45 +14,48 @@ class Column:
         self._data = []
         self._type = []
 
-    def _get_type(self):
+    def _get_type(self, value):
         try:
-            int(self.data)
+            int(value)
             return INT
         except ValueError:
             pass
 
         try:
-            float(self.data)
+            float(value)
             return FLOAT
         except ValueError:
             pass
 
-        if self.data.lower() in ['true', 'false']:
+        if value.lower() in ['true', 'false']:
             return BOOL
         
         try:
-            datetime.strptime(self.data, '%Y-%m-%d')
+            datetime.strptime(value, '%Y-%m-%d')
             return DATE
         except ValueError:
             pass
 
-        if self.data == '':
+        if value == '':
             return NONE
         return STRING  
     
+    def get_column(self, name):
+        return self._columns[name]
+    
     def add_value(self, value):
-        self.type.append(self._get_type())
-        current_type = self._get_type()
+        current_type = self._get_type(value)
         if current_type == INT:
-            self.data.append(int(value))
+            self._data.append(int(value))
         elif current_type == FLOAT:
-            self.data.append(float(value))
+            self._data.append(float(value))
         elif current_type == BOOL:
-            self.data.append(value.lower() == 'true')
+            self._data.append(value.lower() == 'true')
         elif current_type == DATE:
-            self.data.append(datetime.strptime(value, '%Y-%m-%d'))
+            self._data.append(datetime.strptime(value, '%Y-%m-%d'))
         else:
-            self.data.append(value)
+            self._data.append(value)
+        self._type.append(current_type)
     
     def set_column_name(self, name):
         self.name = name
@@ -58,7 +64,39 @@ class Column:
         return self.name
     
     def get_column_data(self):
-        return self.data
+        return self._data
     
     def get_column_data_without_none(self):
-        return [x for x in self.data if x != '']
+        return [x for x in self._data if x != '']
+    
+    def mean(self):
+        values = self.get_column_data_without_none()
+        result = sum(values) / len(values)
+        return round(result, 6)
+    
+    def count(self):
+        return len(self.get_column_data_without_none())
+    
+    def std(self):
+        values = self.get_column_data_without_none()
+        mean = self.mean()
+        result = math.sqrt(sum([(x - mean) ** 2 for x in values]) / (len(values) - 1))
+        return round(result, 6)
+    
+    def min(self):
+        return min(self.get_column_data_without_none())
+    
+    def max(self):
+        return max(self.get_column_data_without_none())
+    
+    def percentile(self, p):
+        values = self.get_column_data_without_none()
+        values.sort()
+        k = (len(values)-1) * p
+        f = math.floor(k)
+        c = math.ceil(k)
+        if f == c:
+            return values[int(k)]
+        d0 = values[int(f)] * (c-k)
+        d1 = values[int(c)] * (k-f)
+        return d0 + d1
---
+++ src/DataFrame.py
@@ -0,0 +1,56 @@
+import csv
+import tabulate
+from Column import Column
+from Row import Row
+import datetime
+
+class DataFrame:
+    def __init__(self):
+        self._columns = {}
+        self._rows = []
+    
+    def add_column(self, name, data):
+        self._columns[name] = Column(name, data)
+    
+    def get_column(self, name):
+        return self._columns[name]
+
+    def read_csv(self, path):
+        with open(path) as file:
+            reader = csv.reader(file)
+            header = next(reader)
+            for column in header:
+                self.add_column(column, [])
+            for row in reader:
+                for column, value in zip(header, row):
+                    self.get_column(column).add_value(value)
+                self._rows.append(Row(header, row))
+    
+    def describe(self):
+        description = {}
+        for header in self._columns:
+            if header == 'Index':
+                continue
+            if isinstance(self._columns[header].get_column_data()[0], str):
+                continue
+            if isinstance(self._columns[header].get_column_data()[0], datetime.datetime):
+                continue
+            description[header] = {
+                'Count' : self._columns[header].count(),
+                'Mean' : self._columns[header].mean(),
+                'Std' : self._columns[header].std(),
+                'Min' : self._columns[header].min(),
+                '25%' : self._columns[header].percentile(0.25),
+                '50%' : self._columns[header].percentile(0.50),
+                '75%' : self._columns[header].percentile(0.75),
+                'Max' : self._columns[header].max()
+            }
+        return description
+    
+    def print_describe(self):
+        description = self.describe()
+        table_data = list(map(list, zip(*[(k, *[format(val, '.6f') for val in v.values()]) for k, v in description.items()])))
+        headers = ['Feature', 'Count', 'Mean', 'Std', 'Min', '25%', '50%', '75%', 'Max']
+        for i in range(len(table_data)):
+            table_data[i].insert(0, headers[i])
+        print(tabulate.tabulate(table_data, tablefmt='pretty'))
